245dcc981f13b0bdb83aa9b638b83e20346e0cf1
Nellie <danielasut@hotmail.com>
2023-05-14 03:31:05 +0800

    Controllers_Article_cs:增加頁面 Views_Article_FAQ_cshtml:改版 Views_Shared_Layout_cshtml:改連結 Models_ADD_Lostpets Views_Article_Add_Lostpets.cshtml Views_Article_delete_Index.cshtml

--- FurryFeast/FurryFeast/Controllers/ArticleController.cs
+++ FurryFeast/FurryFeast/Controllers/ArticleController.cs
@@ -1,13 +1,15 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Drawing;
+using FurryFeast.Models;  // 新添加的行
 
 namespace FurryFeast.Controllers
 {
     public class ArticleController : Controller
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
+        private static readonly HttpClient client = new HttpClient();
+
+
         public IActionResult FAQ()
         {
             return View();
@@ -32,5 +34,22 @@ namespace FurryFeast.Controllers
         {
             return View();
         }
+
+        public async Task<IActionResult> Lostpets()
+        {
+            var jsonString = await client.GetStringAsync("https://data.coa.gov.tw/Service/OpenData/TransService.aspx?UnitId=IFJomqVzyB0i");
+            var pets = JsonConvert.DeserializeObject<List<Lostpets>>(jsonString);
+            // 將每一種可能的寵物種類、品種、毛色、失蹤時間、性別和遺失地點添加到 ViewData
+            ViewData["PetTypes"] = pets.Select(p => p.PetType).Distinct().ToList();
+            ViewData["Breeds"] = pets.Select(p => p.Breed).Distinct().ToList();
+            ViewData["Colors"] = pets.Select(p => p.Color).Distinct().ToList();
+            ViewData["LostTimes"] = pets.Select(p => p.LostTime).Distinct().ToList();
+            ViewData["Genders"] = pets.Select(p => p.Gender).Distinct().ToList();
+            ViewData["LostLocations"] = pets.Select(p => p.LostLocation).Distinct().ToList();
+
+            return View(pets);
+        }
+
     }
 }
+
