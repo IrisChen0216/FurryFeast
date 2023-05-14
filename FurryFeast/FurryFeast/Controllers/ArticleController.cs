69f40da023c95afe0b4b5773b6b9102a6a3c4ec0
Nellie <danielasut@hotmail.com>
2023-05-14 17:02:42 +0800

    Controllers_Article_cs編輯 Views_Article_Lostpets/Shelter:編輯 Models_LostpetsPage新增 Views_LostPetsPArtial新增

--- FurryFeast/FurryFeast/Controllers/ArticleController.cs
+++ FurryFeast/FurryFeast/Controllers/ArticleController.cs
@@ -2,6 +2,7 @@
 using Newtonsoft.Json;
 using System.Drawing;
 using FurryFeast.Models;  // 新添加的行
+using System.Globalization;
 
 namespace FurryFeast.Controllers
 {
@@ -34,12 +35,54 @@ namespace FurryFeast.Controllers
         {
             return View();
         }
-
-        public async Task<IActionResult> Lostpets()
+        public async Task<IActionResult> Lostpets(int? page, string petType, string gender, string breed, string color, DateTime? lostTime, string lostLocation)
         {
+            var pageSize = 12;
+
             var jsonString = await client.GetStringAsync("https://data.coa.gov.tw/Service/OpenData/TransService.aspx?UnitId=IFJomqVzyB0i");
             var pets = JsonConvert.DeserializeObject<List<Lostpets>>(jsonString);
-            // 將每一種可能的寵物種類、品種、毛色、失蹤時間、性別和遺失地點添加到 ViewData
+
+            if (!string.IsNullOrEmpty(petType))
+            {
+                pets = pets.Where(p => p.PetType == petType).ToList();
+            }
+
+            if (!string.IsNullOrEmpty(gender))
+            {
+                pets = pets.Where(p => p.Gender == gender).ToList();
+            }
+
+            if (!string.IsNullOrEmpty(breed))
+            {
+                pets = pets.Where(p => p.Breed.Contains(breed, StringComparison.OrdinalIgnoreCase)).ToList();
+            }
+
+            if (!string.IsNullOrEmpty(color))
+            {
+                pets = pets.Where(p => p.Color.Contains(color, StringComparison.OrdinalIgnoreCase)).ToList();
+            }
+
+            if (lostTime.HasValue)
+            {
+                pets = pets.Where(p => DateTime.TryParseExact(p.LostTime, "yyyy/MM/dd", CultureInfo.InvariantCulture, DateTimeStyles.None, out var pLostTime) && pLostTime.Date == lostTime.Value.Date).ToList();
+            }
+
+            if (!string.IsNullOrEmpty(lostLocation))
+            {
+                pets = pets.Where(p => p.LostLocation.Contains(lostLocation, StringComparison.OrdinalIgnoreCase)).ToList();
+            }
+
+            // 過濾掉沒有有效圖片的資料
+            pets = pets.Where(p => !string.IsNullOrEmpty(p.Picture) && Uri.IsWellFormedUriString(p.Picture, UriKind.Absolute)).ToList();
+
+            var totalItemCount = pets.Count;
+            var totalPageCount = (int)Math.Ceiling(totalItemCount / (double)pageSize);
+
+            var pageNumber = page ?? 1;
+            pageNumber = Math.Max(1, Math.Min(pageNumber, totalPageCount));
+
+            var petsPaged = pets.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToList();
+
             ViewData["PetTypes"] = pets.Select(p => p.PetType).Distinct().ToList();
             ViewData["Breeds"] = pets.Select(p => p.Breed).Distinct().ToList();
             ViewData["Colors"] = pets.Select(p => p.Color).Distinct().ToList();
@@ -47,8 +90,57 @@ namespace FurryFeast.Controllers
             ViewData["Genders"] = pets.Select(p => p.Gender).Distinct().ToList();
             ViewData["LostLocations"] = pets.Select(p => p.LostLocation).Distinct().ToList();
 
-            return View(pets);
+            ViewBag.PageNumber = pageNumber;
+            ViewBag.PageCount = totalPageCount;
+
+            return View(petsPaged);
         }
+
+
+
+        [HttpPost]
+        public async Task<IActionResult> LoadLostPets(string petType, string gender, string breed, string color, DateTime? lostTime, string lostLocation)
+        {
+            var jsonString = await client.GetStringAsync("https://data.coa.gov.tw/Service/OpenData/TransService.aspx?UnitId=IFJomqVzyB0i");
+            var pets = JsonConvert.DeserializeObject<List<Lostpets>>(jsonString);
+
+            if (!string.IsNullOrEmpty(petType))
+            {
+                pets = pets.Where(p => p.PetType == petType).ToList();
+            }
+
+            if (!string.IsNullOrEmpty(gender))
+            {
+                pets = pets.Where(p => p.Gender == gender).ToList();
+            }
+
+            if (!string.IsNullOrEmpty(breed))
+            {
+                pets = pets.Where(p => p.Breed.Contains(breed, StringComparison.OrdinalIgnoreCase)).ToList();
+            }
+
+            if (!string.IsNullOrEmpty(color))
+            {
+                pets = pets.Where(p => p.Color.Contains(color, StringComparison.OrdinalIgnoreCase)).ToList();
+            }
+
+            if (lostTime.HasValue)
+            {
+                pets = pets.Where(p => DateTime.TryParseExact(p.LostTime, "yyyy/MM/dd", CultureInfo.InvariantCulture, DateTimeStyles.None, out var pLostTime) && pLostTime.Date == lostTime.Value.Date).ToList();
+            }
+
+            if (!string.IsNullOrEmpty(lostLocation))
+            {
+                pets = pets.Where(p => p.LostLocation.Contains(lostLocation, StringComparison.OrdinalIgnoreCase)).ToList();
+            }
+
+            // 過濾掉沒有有效圖片的資料
+            pets = pets.Where(p => !string.IsNullOrEmpty(p.Picture) && Uri.IsWellFormedUriString(p.Picture, UriKind.Absolute)).ToList();
+
+            return Json(pets);
+        }
+
+
 
     }
 }
