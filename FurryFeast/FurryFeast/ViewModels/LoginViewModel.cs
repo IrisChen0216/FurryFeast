905d21c698b1deb151d553e93afc9c1d51ae36d3
Lucien <yoyo401010904@gmail.com>
2023-05-19 14:45:01 +0800

    Login_Add/Update_cs/cshtml 新增登入功能 調整Login頁面

--- FurryFeast/FurryFeast/ViewModels/LoginViewModel.cs
+++ FurryFeast/FurryFeast/ViewModels/LoginViewModel.cs
@@ -4,6 +4,8 @@
     {
 		public string MemberAccount { get; set; } = null!;
 		public string MemberPassord { get; set; } = null!;
+		public string AdminAccount { get; set; } = null!;
+		public string AdminPassword { get; set; } = null!;
 
 	}
 }
