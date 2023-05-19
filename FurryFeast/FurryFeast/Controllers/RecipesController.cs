e5ce00508a77dab397485588adf79d11829520f6
Alfie <pilotpen2012@gmail.com>
2023-05-19 16:32:51 +0800

    Recipes_Add_cs/cshtml 新增項目: 1.控制器綁定資料庫 2. addComment 3.食譜畫面修改

--- FurryFeast/FurryFeast/Controllers/RecipesController.cs
+++ FurryFeast/FurryFeast/Controllers/RecipesController.cs
@@ -1,12 +1,37 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FurryFeast.Data;
+using FurryFeast.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FurryFeast.Controllers
 {
 	public class RecipesController : Controller
 	{
+		private readonly db_a989fb_furryfeastContext _context;
+		public RecipesController(db_a989fb_furryfeastContext furryFeastContext)
+		{
+			_context = furryFeastContext;
+		}
 		public IActionResult Recipes()
 		{
 			return View();
 		}
+
+		[HttpPost]
+		[Authorize]
+		public async Task<IActionResult> AddComment(int ID, string UserComment)
+		{
+			var comment = new MsgBoard()
+			{
+				MsgRecipesId = ID,
+				MsgContent = UserComment,
+				UserId = HttpContext.User.Identity.Name, //取得登入中的帳號
+				MsgDateTime = DateTime.Now //取得當下時間
+			};
+			_context.Add(comment);
+			await _context.SaveChangesAsync();
+			return RedirectToAction("Recipes");
+		}
 	}
+
 }
