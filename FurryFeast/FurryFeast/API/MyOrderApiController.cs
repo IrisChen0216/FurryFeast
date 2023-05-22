d15521a6eae7a04ca59cf27caca461de7a01b750
Lucien <yoyo401010904@gmail.com>
2023-05-22 16:06:13 +0800

    Member_Update_cs/cshtml 更新會員中心頁面和我的訂單Api控制器

--- FurryFeast/FurryFeast/API/MyOrderApiController.cs
+++ FurryFeast/FurryFeast/API/MyOrderApiController.cs
@@ -1,11 +1,25 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using FurryFeast.Models;
+using FurryFeast.ViewModels;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Diagnostics;
 
 namespace FurryFeast.API
 {
-    [Route("api/[controller]")]
+    [Route("api/MyOrder/[Action]")]
     [ApiController]
     public class MyOrderApiController : ControllerBase
     {
+        private readonly db_a989fb_furryfeastContext _context;
+        public MyOrderApiController(db_a989fb_furryfeastContext context) 
+        {   
+             _context = context;
+        }
+
+        public Object GetMyOrder(MyOrderViewModel list)
+        {
+
+            return _context.Orders.Select(x=>x.MemberId ==  list.MemberId).ToList();
+        }
     }
 }
