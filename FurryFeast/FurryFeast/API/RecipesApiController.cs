439f65e23ee88ce00546ea508330599e3deefae8
Alfie <pilotpen2012@gmail.com>
2023-05-24 00:23:41 +0800

    Recipies_fix_修正無法抓到資料

---
+++ FurryFeast/FurryFeast/API/RecipesApiController.cs
@@ -0,0 +1,31 @@
+﻿using FurryFeast.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FurryFeast.API
+{
+	[Route("api/recipes/[action]")]
+	[ApiController]
+	public class RecipesApiController : ControllerBase
+	{
+		private readonly db_a989fb_furryfeastContext _context;
+
+		public RecipesApiController(db_a989fb_furryfeastContext context)
+		{
+			_context=context;	
+		}
+
+		public object AllRecipes()
+		{
+			return _context.Recipes.Include(x => x.MsgBoards).Select(x => new
+			{
+				AllRecipes=new
+				{
+					
+					recipesName=x.RecipesName,
+				}
+			});
+		}
+	}
+}
