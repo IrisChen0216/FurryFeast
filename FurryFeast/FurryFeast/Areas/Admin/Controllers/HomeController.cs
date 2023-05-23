99ec917abe9ee380fe177ec8f701d3512fbe9e02
Jerry Chen <pths305@yahoo.com.tw>
2023-05-23 13:02:38 +0800

    加入 Sign Up.

--- FurryFeast/FurryFeast/Areas/Admin/Controllers/HomeController.cs
+++ FurryFeast/FurryFeast/Areas/Admin/Controllers/HomeController.cs
@@ -20,44 +20,44 @@ namespace FurryFeast.Areas.Admin.Controllers {
             return View();
         }
 
-		public IActionResult Map() {
-			return View();
-		}
+        public IActionResult Map() {
+            return View();
+        }
 
-		public IActionResult Notifications() {
-			return View();
-		}
+        public IActionResult Notifications() {
+            return View();
+        }
 
-		public IActionResult Profile() {
-			return View();
-		}
+        public IActionResult Profile() {
+            return View();
+        }
 
-		public IActionResult Rtl() {
-			return View();
-		}
+        public IActionResult Rtl() {
+            return View();
+        }
 
-		public IActionResult SignIn() {
-			return View();
-		}
+        public IActionResult SignIn() {
+            return View();
+        }
 
-		public IActionResult SignUp() {
-			return View();
-		}
+        public IActionResult SignUp() {
+            return View();
+        }
 
-		public IActionResult Tables() {
-			return View();
-		}
+        public IActionResult Tables() {
+            return View();
+        }
 
-		public IActionResult Template() {
-			return View();
-		}
+        public IActionResult Template() {
+            return View();
+        }
 
-		public IActionResult Typography() {
-			return View();
-		}
+        public IActionResult Typography() {
+            return View();
+        }
 
-		public IActionResult VirtualReality() {
-			return View();
-		}
-	}
+        public IActionResult VirtualReality() {
+            return View();
+        }
+    }
 }
