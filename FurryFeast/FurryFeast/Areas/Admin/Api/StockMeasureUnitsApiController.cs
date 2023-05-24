2b6ddbdd9e1c34baca76c8acdf4b39d632177d91
Jerry Chen <pths305@yahoo.com.tw>
2023-05-25 04:00:22 +0800

    編碼喚回UTF8

--- FurryFeast/FurryFeast/Areas/Admin/Api/StockMeasureUnitsApiController.cs
+++ FurryFeast/FurryFeast/Areas/Admin/Api/StockMeasureUnitsApiController.cs
@@ -2,9 +2,10 @@
 using FurryFeast.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace FurryFeast.Areas.Admin.Api {
-	[Route("api/StockMeasureUnitsApiController/[action]")]
+	[Route("api/[controller]/[action]")]
 	[ApiController]
 	public class StockMeasureUnitsApiController : ControllerBase {
 		private db_a989fb_furryfeastContext _context;
@@ -37,16 +38,28 @@ namespace FurryFeast.Areas.Admin.Api {
 		//	}
 		//}
 
+		//[HttpGet]
+		//public async Task<object> GetAll() {
+		//	if (_context.StockMeasureUnits == null) {
+		//		return NotFound();
+		//	} else {
+		//		var result = _context.StockMeasureUnits.Select(data => new StockMeasureUnitsViewModel {
+		//			MeasureUnitsId = data.MeasureUnitsId,
+		//			MeasureUnitsCode = data.MeasureUnitsCode,
+		//			MeasureUnitsDescription = data.MeasureUnitsDescription,
+		//		});
+		//		return Ok(result);
+		//	}
+		//}
+
 		[HttpGet]
 		public async Task<object> GetAll() {
 			if (_context.StockMeasureUnits == null) {
 				return NotFound();
 			} else {
-				var result = _context.StockMeasureUnits.Select(data => new StockMeasureUnitsViewModel {
-					MeasureUnitsId = data.MeasureUnitsId,
-					MeasureUnitsCode = data.MeasureUnitsCode,
-					MeasureUnitsDescription = data.MeasureUnitsDescription,
-				});
+				var result = await _context.StockMeasureUnits.Select(data => new { 
+					All = data
+				}).ToListAsync();
 				return Ok(result);
 			}
 		}
