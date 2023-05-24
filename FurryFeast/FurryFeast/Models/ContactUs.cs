98f2101b0c5853e931c7c35a6c3f9d74785c1fa5
Nellie <danielasut@hotmail.com>
2023-05-24 21:40:56 +0800

    Views_ContactUs_cshtml:新增功能修改功能 Models_ContactUs_Add

---
+++ FurryFeast/FurryFeast/Models/ContactUs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FurryFeast.Models
+{
+    public partial class ContactUs
+    {
+        public int GuestId { get; set; }
+        public string GuestName { get; set; }
+        public string GuestEmail { get; set; }
+        public int? GuestPhone { get; set; }
+        public string GuestSubject { get; set; }
+        public string GuestContext { get; set; }
+    }
+}
