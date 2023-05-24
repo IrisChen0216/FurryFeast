c78510f1416b86199c03711a95d1a6b76aa25d70
Iris <iris850216@gmail.com>
2023-05-24 18:41:28 +0800

    petMarketBackEnd_Add&Update_cs、cshtml_新增商城商品管理頁面商品狀態按鈕、搜尋欄位，以及商品資料匯入

--- FurryFeast/FurryFeast/API/ProductAPIController.cs
+++ FurryFeast/FurryFeast/API/ProductAPIController.cs
@@ -22,7 +22,7 @@ namespace FurryFeast.API
 		
 		public object AllProducts()
 		{
-			return _context.Products.Include(x => x.ProductPics).Include(x => x.ProductType).Select(x => new
+			return _context.Products.Include(x => x.ProductPics).Include(x => x.ProductType).Where(x=>x.ProductState==1).Select(x => new
 			{
 				product = new
 				{
@@ -33,6 +33,7 @@ namespace FurryFeast.API
 					productAmount = x.ProductAmount,
 					productPicId = x.ProductPicId,
 					productLauchedTime = x.ProductLaunchedTime,
+					
 
 				},
 				pics = x.ProductPics.Select(p=>p.ProductPicImage),
@@ -43,6 +44,31 @@ namespace FurryFeast.API
 
 		}
 
-		
-	}
+		//給後臺用的商品
+        public object backEndProducts()
+        {
+						
+            return _context.Products.Include(x => x.ProductPics).Include(x => x.ProductType).Select(x => new
+            {
+                backEndProduct = new
+                {
+                    productId = x.ProductId,
+                    productName = x.ProductName,
+                    productDescription = x.ProductDescription,
+                    productPrice = x.ProductPrice,
+                    productAmount = x.ProductAmount,
+                    productPicId = x.ProductPicId,
+                    productLauchedTime = x.ProductLaunchedTime,
+                    productSoldTime=x.ProductSoldTime,
+                    productState=x.ProductState
+
+                },
+                backEndPics = x.ProductPics.Select(p => p.ProductPicImage),
+                backEndType = x.ProductType.ProductTypeName
+
+            });
+
+
+        }
+    }
 }
