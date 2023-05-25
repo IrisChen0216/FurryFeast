7823d7f83e9f478b8d38e0e1b53e8bf5f7ca89a7
Jerry Chen <pths305@yahoo.com.tw>
2023-05-25 12:06:00 +0800

    Api 新增資料功能

--- FurryFeast/FurryFeast/Areas/Admin/Api/StockMeasureUnitsApiController.cs
+++ FurryFeast/FurryFeast/Areas/Admin/Api/StockMeasureUnitsApiController.cs
@@ -3,6 +3,7 @@ using FurryFeast.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 namespace FurryFeast.Areas.Admin.Api {
 	[Route("api/[controller]/[action]")]
@@ -38,30 +39,41 @@ namespace FurryFeast.Areas.Admin.Api {
 		//	}
 		//}
 
-		//[HttpGet]
-		//public async Task<object> GetAll() {
-		//	if (_context.StockMeasureUnits == null) {
-		//		return NotFound();
-		//	} else {
-		//		var result = _context.StockMeasureUnits.Select(data => new StockMeasureUnitsViewModel {
-		//			MeasureUnitsId = data.MeasureUnitsId,
-		//			MeasureUnitsCode = data.MeasureUnitsCode,
-		//			MeasureUnitsDescription = data.MeasureUnitsDescription,
-		//		});
-		//		return Ok(result);
-		//	}
-		//}
-
+		// 查詢所有資料
 		[HttpGet]
 		public async Task<object> GetAll() {
 			if (_context.StockMeasureUnits == null) {
-				return NotFound();
+				return Problem("StockMeasureUnits is null.");
 			} else {
-				var result = await _context.StockMeasureUnits.Select(data => new { 
-					All = data
+				var result = await _context.StockMeasureUnits.Select(data => new StockMeasureUnitsViewModel {
+					MeasureUnitsId = data.MeasureUnitsId,
+					MeasureUnitsCode = data.MeasureUnitsCode,
+					MeasureUnitsDescription = data.MeasureUnitsDescription,
 				}).ToListAsync();
 				return Ok(result);
 			}
+		}
+
+		// 新增一筆資料
+		[HttpPost]
+		public async Task<object> PostData(StockMeasureUnitsViewModel data) {
+			if (_context.StockMeasureUnits == null) {
+				return Problem("StockMeasureUnits is null.");
+			}
+
+			// 如果資料重複
+			if (_context.StockMeasureUnits?.Any(e => e.MeasureUnitsCode == data.MeasureUnitsCode) == true) {
+				return Problem($"Data duplicate, Code: {data.MeasureUnitsCode}");
+			}
+
+			StockMeasureUnit result = new StockMeasureUnit {
+				MeasureUnitsId = data.MeasureUnitsId,
+				MeasureUnitsCode = data.MeasureUnitsCode,
+				MeasureUnitsDescription = data.MeasureUnitsDescription,
+			};
+			_context.StockMeasureUnits?.Add(result);
+			await _context.SaveChangesAsync();
+			return Ok($"Post success, Code: {data.MeasureUnitsCode}.");
 		}
 	}
 }
