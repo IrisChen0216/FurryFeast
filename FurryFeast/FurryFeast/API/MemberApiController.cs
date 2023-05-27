aa088bf10cf608111cd2e76b96a9d079db25ca02
Lucien <yoyo401010904@gmail.com>
2023-05-27 17:41:40 +0800

    Member_Add/Update_cs/cshtml 新增會員資訊&會員管理、修改標題樣式

--- FurryFeast/FurryFeast/API/MemberApiController.cs
+++ FurryFeast/FurryFeast/API/MemberApiController.cs
@@ -1,38 +1,41 @@
 ﻿using FurryFeast.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore.Infrastructure.Internal;
 
 namespace FurryFeast.API
 {
-	[Route("api/Member/[action]")]
-	[ApiController]
-	public class MemberApiController : ControllerBase
-	{
-		private readonly db_a989fb_furryfeastContext _context;
+    [Authorize]
+    [Route("/api/Member/[action]")]
+    [ApiController]
+    public class MemberApiController : ControllerBase
+    {
+        private readonly db_a989fb_furryfeastContext _context;
 
-		public MemberApiController(db_a989fb_furryfeastContext context)
-		{
-			_context = context;
-		}
+        public MemberApiController(db_a989fb_furryfeastContext context)
+        {
+            _context = context;
+        }
 
-		public object member()
-		{
-			return _context.Members.Select(x => new 
 
-			{
-				member= new
-				{
-					memberName = x.MemberName,
-					memberBirthday = x.MemberBirthday,
-					memberGender = x.MemberGender,
-					memberPhone = x.MemberPhone,
-					memberEmail = x.MemberEmail,
-					memberAdress = x.MemberAdress,
-					memberId = x.MemberId
-				}
+        public object All()
+        {
+            return _context.Members.Select(x => new
 
-			});
-		}
-	}
+            {
+                member = new
+                {
+                    memberName = x.MemberName,
+                    memberBirthday = x.MemberBirthday,
+                    memberGender = x.MemberGender,
+                    memberPhone = x.MemberPhone,
+                    memberEmail = x.MemberEmail,
+                    memberAdress = x.MemberAdress,
+                    memberId = x.MemberId
+                }
+
+            });
+        }
+    }
 }
