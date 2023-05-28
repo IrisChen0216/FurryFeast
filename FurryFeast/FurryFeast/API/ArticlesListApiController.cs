68a3afab20b142a9656ee777717f4cc378e4c791
Alfie <pilotpen2012@gmail.com>
2023-05-28 12:05:37 +0800

    no message

---
+++ FurryFeast/FurryFeast/API/ArticlesListApiController.cs
@@ -0,0 +1,20 @@
+﻿using FurryFeast.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FurryFeast.API
+{
+	[Route("api/Member/[action]")]
+	[ApiController]
+	public class ArticlesListController : ControllerBase
+	{
+		private readonly db_a989fb_furryfeastContext _context;
+
+		public ArticlesListController(db_a989fb_furryfeastContext context)
+		{
+			_context = context;
+		}
+	}
+
+
+}
---
+++ FurryFeast/FurryFeast/wwwroot/backend/js/backEndArticleCreate.js
@@ -0,0 +1,4 @@
+﻿import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+
+// Or using the CommonJS version:
+// const ClassicEditor = require( '@ckeditor/ckeditor5-build-classic' );
