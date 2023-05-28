6e092523263b70a841326a0b221b23f01243d35a
Alfie <pilotpen2012@gmail.com>
2023-05-28 18:03:19 +0800

    新增會員資訊api

--- FurryFeast/FurryFeast/API/RecipesApiController.cs
+++ FurryFeast/FurryFeast/API/RecipesApiController.cs
@@ -1,104 +1,129 @@
 ﻿using FurryFeast.Models;
 using FurryFeast.ViewModels;
-using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.CodeAnalysis.VisualBasic.Syntax;
 using Microsoft.EntityFrameworkCore;
 
-namespace FurryFeast.API
+namespace FurryFeast.API;
+
+[Route("api/recipes/[action]")]
+[ApiController]
+public class RecipesApiController : ControllerBase
 {
-	[Route("api/recipes/[action]")]
-	[ApiController]
-	public class RecipesApiController : ControllerBase
+	private readonly db_a989fb_furryfeastContext _context;
+
+	public RecipesApiController(db_a989fb_furryfeastContext context)
 	{
-		private readonly db_a989fb_furryfeastContext _context;
-
-		public RecipesApiController(db_a989fb_furryfeastContext context)
-		{
-			_context = context;
-		}
-
-		public object AllData()
-		{
-			return _context.Recipes
-				.Include(x => x.MsgBoards)
-				.ThenInclude(m => m.EditedMsgRecords)
-				.Select(x => new
-				{
-					AllData = new
-					{
-						name = x.RecipesName,
-						recipesId = x.RecipesId,
-						petTypesId = x.PetTypesId,
-						desc = x.RecipesDescription,
-						data = x.RecipesData,
-						method = x.RecipesMethod,
-						notes = x.RecipesNotes,
-						MsgBoards = x.MsgBoards.Select(mb => new
-						{
-							msgId = mb.MsgId,
-							recipesId = mb.MsgRecipesId,
-							userId = mb.UserId,
-							Content = mb.MsgContent,
-							DateTime = mb.MsgDateTime,
-							Active = mb.MsgActive,
-							EditedMsgRecords = mb.EditedMsgRecords.Select(emr => new
-							{
-								editedMsgId = emr.EditedMsgId,
-								msgId = emr.MsgId,
-								editedText = emr.EditedText,
-								editedTime = emr.EditedTime
-							})
-
-						})
-					}
-
-				});
-		}
-
-		[HttpPost]
-		public async Task<ActionResult<string>> AddComment([FromBody] MsgBoardViewModel msgboard)
-		{
-			if (!string.IsNullOrEmpty(HttpContext.User.Identity.Name))
-			{
-				msgboard.UserId = HttpContext.User.Identity.Name;
-			}
-			else
-			{
-				msgboard.UserId = "unknown";
-			}
-
-			MsgBoard newComment = new MsgBoard()
-			{ 
-				UserId = msgboard.UserId,
-				MsgRecipesId = msgboard.MsgRecipesId, 
-				MsgContent = msgboard.MsgContent,           //留言內容
-				MsgDateTime = msgboard.MsgDateTime,  //時間
-				MsgActive = msgboard.MsgActive           //狀態	
-			}; 
-			_context.Add(newComment);
-			await _context.SaveChangesAsync();
-			return $"success";
-
-		}
-
-		//Edit Update
-		[HttpPost]
-		public async Task<ActionResult<string>> EditedMsg([FromBody] EditedMsgRecordViewModel editedMsgRecord)
-		{
-
-			EditedMsgRecord editedComment = new EditedMsgRecord()
-			{
-				MsgId = editedMsgRecord.MsgId,
-				EditedText = editedMsgRecord.EditedText, 
-				EditedTime = editedMsgRecord.EditedTime,
-			};
-			_context.Add(editedComment);
-			await _context.SaveChangesAsync();
-			return $"success";
-
-		}
-
+		_context = context;
 	}
 
+	public object AllData()
+	{
+		return _context.Recipes
+			.Include(x => x.MsgBoards)
+			.ThenInclude(m => m.EditedMsgRecords)
+			.Select(x => new
+			{
+				AllData = new
+				{
+					name = x.RecipesName,
+					recipesId = x.RecipesId,
+					petTypesId = x.PetTypesId,
+					desc = x.RecipesDescription,
+					data = x.RecipesData,
+					method = x.RecipesMethod,
+					notes = x.RecipesNotes,
+					MsgBoards = x.MsgBoards.Select(mb => new
+					{
+						msgId = mb.MsgId,
+						recipesId = mb.MsgRecipesId,
+						userId = mb.UserId,
+						Content = mb.MsgContent,
+						DateTime = mb.MsgDateTime,
+						Active = mb.MsgActive,
+						EditedMsgRecords = mb.EditedMsgRecords.Select(emr => new
+						{
+							editedMsgId = emr.EditedMsgId,
+							msgId = emr.MsgId,
+							editedText = emr.EditedText,
+							editedTime = emr.EditedTime
+						})
+					})
+				}
+			});
+	}
+
+	//[AllowAnonymous]
+	public ApiResponseModel GetUserId()
+	{
+		var user = User.Claims.FirstOrDefault(x => x.Type == "Id");
+		if (user == null)
+		{
+			return new ApiResponseModel() { Status = false };
+		}
+
+		var id = int.Parse(user.Value);
+		var person = _context.Members.FirstOrDefault(x => x.MemberId == id);
+		if (person == null)
+		{
+			return new ApiResponseModel() { Status = false };
+		}
+		return new ApiResponseModel()
+		{
+			Status = true,
+			Data = new
+			{
+				person.MemberId,
+				person.MemberName,
+				person.MemberBirthday,
+				person.MemberAdress,
+				person.MemberEmail,
+				person.MemberPhone,
+				person.MemberGender,
+				person.MemberAccount,
+				person.MemberPassord,
+				person.ConponId
+
+			}
+		};
+	}
+
+	[HttpPost]
+	[Authorize]
+	public async Task<ActionResult<string>> AddComment([FromBody] MsgBoardViewModel msgboard)
+	{
+		//var userId = User.Claims.FirstOrDefault(x => x.Type == "Id").Value;
+
+		if (!string.IsNullOrEmpty(HttpContext.User.Identity.Name))
+			msgboard.UserId = HttpContext.User.Identity.Name;
+		else
+			msgboard.UserId = "unknown";
+
+		var newComment = new MsgBoard
+		{
+			UserId = msgboard.UserId,
+			MsgRecipesId = msgboard.MsgRecipesId,
+			MsgContent = msgboard.MsgContent, //留言內容
+			MsgDateTime = msgboard.MsgDateTime, //時間
+			MsgActive = msgboard.MsgActive //狀態	
+		};
+		_context.Add(newComment);
+		await _context.SaveChangesAsync();
+		return "success";
+	}
+
+	//Edit Update
+	[HttpPost]
+	public async Task<ActionResult<string>> EditedMsg([FromBody] EditedMsgRecordViewModel editedMsgRecord)
+	{
+		var editedComment = new EditedMsgRecord
+		{
+			MsgId = editedMsgRecord.MsgId,
+			EditedText = editedMsgRecord.EditedText,
+			EditedTime = editedMsgRecord.EditedTime
+		};
+		_context.Add(editedComment);
+		await _context.SaveChangesAsync();
+		return "success";
+	}
 }
---
+++ FurryFeast/FurryFeast/ViewModels/ApiResponseModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace FurryFeast.ViewModels
+{
+	public class ApiResponseModel
+	{
+		public bool Status { get; set; }
+		public object Data { get; set; }
+	}
+}
