a448e4b816e4fc04ea846facb3b2ac035aea25ba
Iris <iris850216@gmail.com>
2023-05-29 20:03:09 +0800

    PetMarket_CartOrder_新增訂單資料介面、將購物車資料和會員資料帶入訂單資料頁面

--- FurryFeast/FurryFeast/Controllers/CartOrderController.cs
+++ FurryFeast/FurryFeast/Controllers/CartOrderController.cs
@@ -1,7 +1,10 @@
-﻿using FurryFeast.Helper;
+﻿using FurryFeast.API;
+using FurryFeast.Helper;
 using FurryFeast.Models;
 using FurryFeast.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
 
 namespace FurryFeast.Controllers
 {
@@ -12,25 +15,54 @@ namespace FurryFeast.Controllers
 		public CartOrderController(db_a989fb_furryfeastContext context) { 
 			_context=context;
 		}
-		public IActionResult CheckOrder()
+
+        public string GetUserId()
+        {
+            return User.FindFirstValue("Id");
+        }
+
+        public IActionResult CheckOrder()
 		{
 			if (SessionHelper.GetProductCartSession<List<CartItem>>(HttpContext.Session, "cart")==null)
 			{
 				RedirectToAction("ProductCart", "Products");
 			}
 
-			var user= User.Claims.FirstOrDefault(x => x.Type == "id").Value;
-			int userID = int.Parse(user);
+			
+			int userID=int.Parse(GetUserId());
 
-            var memberOrder = new Order
-			{
-				MemberId = userID,
-				//MemberName = _context.Members.Where(x=>x.MemberId==userID)
-				OrderDetails = SessionHelper.GetProductCartSession<List<CartItem>>(HttpContext.Session, "cart").ToArray(),
+            var person = _context.Members.FirstOrDefault(x => x.MemberId == userID);
 
-			};
-			memberOrder.OrderTotalPrice = memberOrder.OrderDetails.Sum(x=>x.OrderPrice);
-			return View(memberOrder);
+            var memberOrder = _context.Orders.Include(x => x.Member).Where(x => x.MemberId == userID).Select(x => new CartOrderViewModel
+            {
+                Order = new Order
+                {
+                    MemberId = x.MemberId,
+                    OrderDetails = SessionHelper.GetProductCartSession<List<CartItem>>(HttpContext.Session, "cart").ToArray(),
+                    OrderTotalPrice = x.OrderTotalPrice
+                },
+                Member =new Member
+                {
+                    MemberName = x.Member.MemberName,
+                    MemberEmail = x.Member.MemberEmail,
+                    MemberPhone = x.Member.MemberPhone,
+                }
+                
+            }).FirstOrDefault();
+            //         var memberOrder = new Order
+            //{
+            //	MemberId = person.MemberId,
+            //	//MemberName = _context.Members.Where(x=>x.MemberId==userID)
+            //	OrderDetails = SessionHelper.GetProductCartSession<List<CartItem>>(HttpContext.Session, "cart").ToArray(),
+
+
+            //};
+
+
+            //memberOrder = memberOrder.OrderDetails.Sum(x=>x.OrderPrice);
+			ViewBag.CartItem = SessionHelper.GetProductCartSession<List<CartItem>>(HttpContext.Session, "cart");
+
+            return View(memberOrder);
 		}
 	}
 }
