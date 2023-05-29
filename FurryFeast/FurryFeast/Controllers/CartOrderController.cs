e187b3b60888a504d9c38e9b7197ff74f0800e55
Iris <iris850216@gmail.com>
2023-05-29 17:25:13 +0800

    PetMarker_CartOrder_修改購物車模型，嘗試抓取登入會員ID

---
+++ FurryFeast/FurryFeast/Controllers/CartOrderController.cs
@@ -0,0 +1,36 @@
+﻿using FurryFeast.Helper;
+using FurryFeast.Models;
+using FurryFeast.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FurryFeast.Controllers
+{
+	public class CartOrderController : Controller		
+	{
+		private readonly db_a989fb_furryfeastContext _context;
+
+		public CartOrderController(db_a989fb_furryfeastContext context) { 
+			_context=context;
+		}
+		public IActionResult CheckOrder()
+		{
+			if (SessionHelper.GetProductCartSession<List<CartItem>>(HttpContext.Session, "cart")==null)
+			{
+				RedirectToAction("ProductCart", "Products");
+			}
+
+			var user= User.Claims.FirstOrDefault(x => x.Type == "id").Value;
+			int userID = int.Parse(user);
+
+            var memberOrder = new Order
+			{
+				MemberId = userID,
+				//MemberName = _context.Members.Where(x=>x.MemberId==userID)
+				OrderDetails = SessionHelper.GetProductCartSession<List<CartItem>>(HttpContext.Session, "cart").ToArray(),
+
+			};
+			memberOrder.OrderTotalPrice = memberOrder.OrderDetails.Sum(x=>x.OrderPrice);
+			return View(memberOrder);
+		}
+	}
+}
--- FurryFeast/FurryFeast/ViewModels/CartItem.cs
+++ FurryFeast/FurryFeast/ViewModels/CartItem.cs
@@ -3,12 +3,12 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace FurryFeast.ViewModels
 {
-    public class CartViewModel
+    public class CartItem:OrderDetail
     {
-        public int ProductID { get; set; }
-        public int Amount { get; set; }
+        //public int ProductID { get; set; }
+        //public int Amount { get; set; }
         public int Subtotal { get; set; }
-        public int Price { get; set; }
+        //public int Price { get; set; }
         public string ProductName { get; set; }
         public string ProductImage { get; set; }
     }
