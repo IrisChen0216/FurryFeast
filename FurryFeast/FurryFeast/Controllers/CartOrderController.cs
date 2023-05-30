5da04dd89fbe5e21b66010f1ac2b67a4ce5e221f
Iris <iris850216@gmail.com>
2023-05-30 10:48:34 +0800

    PetMarket_CartOrder_訂單和訂單明細成功寫進資料庫，但是是直接從購物車撈，購物車目前不能挑選

--- FurryFeast/FurryFeast/Controllers/CartOrderController.cs
+++ FurryFeast/FurryFeast/Controllers/CartOrderController.cs
@@ -47,6 +47,7 @@ namespace FurryFeast.Controllers
                     MemberName = x.Member.MemberName,
                     MemberEmail = x.Member.MemberEmail,
                     MemberPhone = x.Member.MemberPhone,
+                    
                 }
                 
             }).FirstOrDefault();
@@ -66,20 +67,49 @@ namespace FurryFeast.Controllers
             return View(memberOrder);
 		}
 
-        public async Task<IActionResult> AddOrder(Order order)
+        public async Task<IActionResult> AddOrder(AddOrderOrderDetailsViewModel model)
         {
-            
-				if (ModelState.IsValid)
-				{
-					order.OrderCreateDate = DateTime.Now;
-					order.OrderShipDate = DateTime.Now.AddDays(3);
-					order.OrderDetails = SessionHelper.GetProductCartSession<List<CartItem>>(HttpContext.Session, "cart").ToArray();
-					order.OrderStatus = 0;
+            Order order = new Order
+            {
+                OrderCreateDate = DateTime.Now,
+                OrderShipDate= DateTime.Now.AddDays(3),
+				OrderRecipientName=model.OrderRecipientName,
+                OrderRecipientPhone=model.OrderRecipientPhone,
+                OrderTotalPrice=model.OrderTotalPrice,
+                OrderRecipientAdress=model.OrderRecipientAdress,
+                OrderStatus=0,
+                MemberId=model.MemberId,
+			};
 
-					_context.Orders.Add(order);
-					await _context.SaveChangesAsync();
-					return RedirectToAction("Index");
-				}
+			_context.Orders.Add(order);
+			await _context.SaveChangesAsync();
+
+			List<CartItem> cartItems = SessionHelper.GetProductCartSession<List<CartItem>>(HttpContext.Session, "cart");
+
+            foreach(CartItem item in cartItems)
+            {
+				OrderDetail orderDetail = new OrderDetail
+				{
+                    OrderPrice = item.OrderPrice,
+                    OrderQuantity= item.OrderQuantity,
+                    ProductId= item.ProductId,
+                    OrderId = order.OrderId,
+                };
+				_context.OrderDetails.Add(orderDetail);
+			};
+			await _context.SaveChangesAsync();
+
+			//if (ModelState.IsValid)
+			//	{
+					//order.OrderCreateDate = DateTime.Now;
+					//order.OrderShipDate = DateTime.Now.AddDays(3);
+					//order.OrderDetails = SessionHelper.GetProductCartSession<List<CartItem>>(HttpContext.Session, "cart").ToArray();
+					//order.OrderStatus = 0;
+
+				//	_context.Add(order);
+				//	await _context.SaveChangesAsync();
+				//	return RedirectToAction("Index");
+				//}
 			
 			return View();
         }
---
+++ FurryFeast/FurryFeast/ViewModels/AddOrderOrderDetailsViewModel.cs
@@ -0,0 +1,20 @@
+﻿namespace FurryFeast.ViewModels
+{
+	public class AddOrderOrderDetailsViewModel
+	{
+		public int OrderId { get; set; }
+		public DateTime OrderCreateDate { get; set; }
+		public DateTime OrderShipDate { get; set; }
+		public string OrderRecipientName { get; set; } = null!;
+		public string OrderRecipientAdress { get; set; } = null!;
+		public string OrderRecipientPhone { get; set; } = null!;
+		public int OrderTotalPrice { get; set; }
+		public int? OrderStatus { get; set; }
+		public int MemberId { get; set; }
+
+		public int OrderDetailId { get; set; }
+		public int OrderPrice { get; set; }
+		public int OrderQuantity { get; set; }
+		public int ProductId { get; set; }
+	}
+}
