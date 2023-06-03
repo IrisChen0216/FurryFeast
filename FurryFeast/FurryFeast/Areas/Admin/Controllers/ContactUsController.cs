63aac9196feea3bab24823f75a0642d3b96162f9
Nellie <danielasut@hotmail.com>
2023-06-03 10:49:04 +0800

    新增控制器跟聯絡我們後台

---
+++ FurryFeast/FurryFeast/Areas/Admin/Controllers/ContactUsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FurryFeast.Areas.Admin.Controllers
+{
+	public class ContactUsController : Controller
+	{
+		public IActionResult ContactUs()
+		{
+			return View();
+		}
+	}
+}
