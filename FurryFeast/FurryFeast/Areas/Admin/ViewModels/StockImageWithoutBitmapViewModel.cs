4c65ef2d6f03b5e7a8af7d30a4d8be0c6d7af92f
Jerry Chen <pths305@yahoo.com.tw>
2023-06-04 12:01:42 +0800

    新增一個圖片的 view model 不需要 bitmap

---
+++ FurryFeast/FurryFeast/Areas/Admin/ViewModels/StockImageWithoutBitmapViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FurryFeast.Areas.Admin.ViewModels {
+    public class StockImageWithoutBitmapViewModel {
+        [Required]
+        public int ImagesId { get; set; }
+        [Required]
+        public string ImagesCode { get; set; } = null!;
+        [Required]
+        public string ImagesDescription { get; set; } = null!;
+        [Required]
+        public int ImagesFileCrc { get; set; }
+    }
+}
