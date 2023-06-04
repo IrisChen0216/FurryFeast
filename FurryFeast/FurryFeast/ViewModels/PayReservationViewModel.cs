2bd865390cf5b43ff44c6aa85ef7f657b32d1458
Iris <iris850216@gmail.com>
2023-06-04 11:36:50 +0800

    寵物課程_已可以送到金流頁面，還沒做付款完成和失敗的畫面

--- FurryFeast/FurryFeast/ViewModels/PayReservationViewModel.cs
+++ FurryFeast/FurryFeast/ViewModels/PayReservationViewModel.cs
@@ -1,11 +1,9 @@
-﻿using Newtonsoft.Json.Linq;
-
-namespace FurryFeast.ViewModels
+﻿namespace FurryFeast.ViewModels
 {
-    public class AddReservationViewModel
-    {      
+    public class PayReservationViewModel
+    {
         public int MemberId { get; set; }
-        public int PetClassId { get; set; }
+        //public int PetClassId { get; set; }
         public string PetClassName { get; set; }
         public int PetClassPrice { get; set; }
         public DateTime PetClassDate { get; set; }
@@ -14,6 +12,9 @@ namespace FurryFeast.ViewModels
         public string MemberName { get; set; }
         public string MemberEmail { get; set; }
         public string MemberPhone { get; set; }
-
+        public int ClassReservetionId { get; set; }
+        public DateTime ClassReservetionDate { get; set; }
+        public int ClassReservetionState { get; set; }
+        
     }
 }
