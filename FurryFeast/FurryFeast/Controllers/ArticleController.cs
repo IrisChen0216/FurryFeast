a59211475953ab9c02779e34e1ac264d9221f48c
Nellie <danielasut@hotmail.com>
2023-06-06 18:49:48 +0800

    改了Donate控制器跟Donates頁面連資料庫

--- FurryFeast/FurryFeast/Controllers/ArticleController.cs
+++ FurryFeast/FurryFeast/Controllers/ArticleController.cs
@@ -57,8 +57,13 @@ namespace FurryFeast.Controllers {
 
 
 		public IActionResult Donates() {
-			var donate = _furryFeastContext.Donates.FirstOrDefault();
-			return View(donate);
+		
+			
+			int order = _furryFeastContext.OrderDetails.Where(x => x.Product.ProductTypeId == 2 && x.Order.OrderStatus==0).Count();
+			int price = _furryFeastContext.OrderDetails.Where(x => x.Product.ProductTypeId == 4 && x.Order.OrderStatus == 0).Sum(x=>x.Product.ProductPrice);
+			ViewBag.DonateNum=order;
+			ViewBag.CouponNum=price;
+			return View();
 		}
 
 		public async Task<IActionResult> Shelter() {
